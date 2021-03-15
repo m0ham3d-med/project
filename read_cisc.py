ebc339a03e71fa8fd86be56026c6522430b8fc1f
m0ham3d-med <med.bsq2@gmail.com>
2021-03-15 14:48:32 +0100

    15 مارس, 2021 UTC 01:48:32 م

---
+++ read_cisc.py
@@ -0,0 +1,46 @@
+import csv
+
+
+def read_cisc_row(filename ):
+    Lines = open(filename, 'r').readlines()
+    array = []
+    req = []
+    for line in Lines :
+        line = line.replace("\n", "")
+        if "POST " in line or "GET " in line:
+            array.append(" ".join(req))
+            req = []
+            req.append(line)
+        else :
+            req.append(line)
+    return array
+
+def add_to_csv_file(filename,data):
+    with open(filename, 'a+', newline='') as out:
+            file_write = csv.writer(out, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            file_write.writerow(data)
+
+def request_to_bow(text):
+        text =text.lower()
+        total_length  = len(text.split()) 
+        if total_length  == 0 :
+            return [0] * 64
+        bag =[]
+        words = [ '<', '../', 'alert', 'exec', 'password', '<>', '’', 'alter', 'from', 'path/child', '<!–', '“', 'and', 'href', 'script', '=', '(', 'bash_history', '#include', 'select', '>', ')', 'between', 'insert', 'shell', '|', '$', '/c', 'into', 'table', '||', '*', 'cmd', 'javascript:', 'union', '-', '*/', 'cn=', 'mail=', 'upper', '–>', '&', 'commit', 'objectclass', 'url=', ';', '+', 'count', 'onmouseover', 'User-Agent:', ':', '%00', '-craw', 'or', 'where', '/', '%0a', 'document.cookie', 'order', 'winnt', '/*', 'Accept:', 'etc/passwd', 'passwd', ]
+        for item  in words :
+            repeat_nbr = text.count(item)
+            value =  repeat_nbr / total_length
+            bag.append(value)
+        return bag 
+
+
+
+csv_filename  = "test.csv"
+cisc_filename = "normalTrafficTraining.txt"
+request_list =read_cisc_row(cisc_filename)
+
+for req in request_list :
+    bag = request_to_bow(str(req))
+    bag.append(0)
+    add_to_csv_file(csv_filename,bag)
+
