b63ae8aa2237809be32f2097f5d8b032e6dfca8c
m0ham3d-med <med.bsq2@gmail.com>
2021-04-06 11:00:45 +0100

    06 أفريل, 2021 UTC 10:00:45 ص

---
+++ bow.py
@@ -0,0 +1,37 @@
+
+import csv
+
+
+def add_to_csv_file(filename,data):
+    with open(filename, 'a+', newline='') as out:
+            file_write = csv.writer(out, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            file_write.writerow(data)
+
+def request_to_bow(text):
+        text =text.lower()
+        total_length  = len(text.split()) 
+        if total_length  == 0 :
+            return [0] * 64
+        bag =[]
+        words = [ '<', '../', 'alert', 'exec', 'password', '<>', '’', 'alter', 'from', 'path/child', '<!–', '“', 'and', 'href', 'script', '=', '(', 'bash_history', '#include', 'select', '>', ')', 'between', 'insert', 'shell', '|', '$', '/c', 'into', 'table', '||', '*', 'cmd', 'javascript:', 'union', '-', '*/', 'cn=', 'mail=', 'upper', '–>', '&', 'commit', 'objectclass', 'url=', ';', '+', 'count', 'onmouseover', 'User-Agent:', ':', '%00', '-craw', 'or', 'where', '/', '%0a', 'document.cookie', 'order', 'winnt', '/*', 'Accept:', 'etc/passwd', 'passwd', ]
+        for item  in words :
+            repeat_nbr = text.count(item)
+            value =  repeat_nbr / total_length
+            bag.append(value)
+        return bag 
+
+
+
+file1 = open("global_dataset.csv", "r")
+csv_filename = "ml_dataset.csv"
+
+for line in file1:
+    bag = request_to_bow(str(line))
+    if "attack" in line:
+        bag.append(1)
+    else:
+        bag.append(0)
+    add_to_csv_file(csv_filename,bag)
+    
+file1.close()
+
